94209af0e0b8dd6928c9cb85e3b0a7f6cb6da523
ThomasPascual <thoma1@free.fr>
2020-07-22 21:21:01 +0200

    functionality:add a friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "following friend"
+    else
+      flash[:alert]  = "there was something wrong with tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
